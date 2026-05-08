6151ac1eac1a915f40dc8ebc3614ef2ac5c41533
minhvq1308 <vm1840@gmail.com>
2026-05-09 01:43:56 +0700

    Bai 4 has been completed

---
+++ Assignment_1/Ex_1/Solving/1.4/Bai4.hpp
@@ -0,0 +1,9 @@
+#ifndef BAI4_HPP
+#define BAI4_HPP
+
+double Sum(double a, double b);
+double Sub(double a, double b);
+double Mul(double a, double b);
+double Div(double a, double b);
+
+#endif
---
+++ Assignment_1/Ex_1/Solving/1.4/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include "Bai4.hpp"
+
+int main() {
+    double num1, num2;
+    double result;
+    char dau;
+
+    std::cin >> num1;
+    std::cin >> num2;
+    std::cin >> dau;
+
+   
+
+    if(dau == '+') {
+        result = Sum(num1, num2);
+    }
+    else if(dau == '-') {
+        result = Sub(num1, num2);
+    }
+    else if(dau == '*') {
+        result = Mul(num1, num2);
+    }
+    else if(dau == '/') {
+        if(num2 != 0) {
+        result = Div(num1, num2);
+    }   else{
+        return 0;
+    }
+    }
+    else{
+        return 0;
+    }
+    std::cout << result << std::endl;
+    return 0;
+}
