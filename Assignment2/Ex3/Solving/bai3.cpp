8ccdf8eebfce11b026d4d4589c81057d44a82db3
minhvq1308 <vm1840@gmail.com>
2026-07-12 16:46:10 +0700

    Bai3 lan 2

--- Assignment2/Ex3/Solving/bai3.cpp
+++ Assignment2/Ex3/Solving/bai3.cpp
@@ -1,10 +1,24 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
 
-int main() {
-    int anArray[9] = { 4, 6, 7, 3, 8, 2, 1, 9, 5 };
-    int *ptr = anArray;
-    for(int i = 0; i < 9; i++) {
-        std::cout << *(ptr+i) << " ";
+int main()
+{
+    int *ptr = new int[9]{4, 6, 7, 3, 8, 2, 1, 9, 5};
+
+    if (ptr == nullptr)
+    {
+        cout << "fail";
     }
+    else{
+
+
+    for (int i = 0; i < 9; i++)
+    {
+        cout << *(ptr + i) << " ";
+    }
+    }
+    delete[] ptr;
+    
+
     return 0;
 }
